880a7a5bd601f753e148e22d4ae7dbe70fd13e45
Cheese-Slice-1 <110117868+Cheese-Slice-1@users.noreply.github.com>
2025-02-02 17:18:37 +0100

    yep

--- src/Engine.hx
+++ src/Engine.hx
@@ -1,5 +1,17 @@
 package ;
 
+enum Note {
+    A(o:Int);
+    B(o:Int);
+    C(o:Int);
+    D(o:Int);
+    E(o:Int);
+    F(o:Int);
+    G(o:Int);
+}
+
 class Engine {
-    
+    public function say(note: Note) {
+        
+    }
 }
