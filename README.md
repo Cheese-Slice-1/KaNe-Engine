35dee3a41978f69470153a187d5629349eba4c6c
Cheese-Slice-1 <110117868+Cheese-Slice-1@users.noreply.github.com>
2025-02-02 15:30:53 +0100

    haxeui(-html5) base UI

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # KaNe Engine
-A simple, similar-to-utau engine that works using only stylized (.kane) text files.
+A simple, similar-to-utau engine that works using only stylized (.kane.txt) text files.
 
 ## Porting/creating voices
 To port voices you must have your samples in WAV format. Then, you create a .yaml file with this formatting:
+
+```yaml
+name: Singer's name
+image: image.bmp
+description: Your singer's description.
+pitch: c4 #change the pitch accordingly
+jpn-ro: #for romaji, change the alias' parameters accordingly
+    phonemes:
+        a: #alias
+            source: source.wav
+            offset: 6 #start of sample
+            consonant: 52 #consonant area
+            cutoff: 69 #end of sample
+            preutterance: 0 #consonant-vowel limit
+        "*a":
+            source: source.wav
+            offset: 16
+            consonant: 52
+            cutoff: 69
+            preutterance: 26
+```
---
+++ src/MainView.hx
@@ -0,0 +1,40 @@
+package ;
+
+import haxe.ui.containers.dialogs.Dialog.DialogButton;
+import haxe.ui.containers.dialogs.Dialogs.FileDialogTypes;
+import haxe.ui.containers.VBox;
+import haxe.ui.events.MouseEvent;
+import haxe.ui.containers.dialogs.OpenFileDialog;
+
+
+@:build(haxe.ui.ComponentBuilder.build("assets/main-view.xml"))
+class MainView extends VBox {    
+    public function new() {
+        super();
+    }
+    
+    @:bind(openKaNeFile, MouseEvent.CLICK)
+    private function onMyButton(_) {
+        var dialog = new OpenFileDialog();
+        dialog.options = {
+            readContents: true,
+            title: "Open KaNe File",
+            readAsBinary: true,
+            extensions: FileDialogTypes.TEXTS
+        };
+        dialog.onDialogClosed = function(event) {
+            var file: String;
+            if (event.button == DialogButton.OK) {
+                file = (dialog.selectedFiles[0].text).toString();
+            } else {
+                file = "...";
+            }
+            if (file == "undefined") {
+                fileLabel.text = "Something went wrong...";
+            } else {
+                fileLabel.text = file;
+            }
+        }
+        dialog.show();
+    }
+}
