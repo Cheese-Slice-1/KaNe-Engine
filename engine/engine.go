ba642f8bfb99c185cd46ada2e4f136c69aec58bc
Cheese-Slice-1 <110117868+Cheese-Slice-1@users.noreply.github.com>
2025-12-08 12:04:57 +0100

    Changed engine a bit

--- engine/engine.go
+++ engine/engine.go
@@ -7,27 +7,31 @@ import (
 	"resampler"
 	"stitcher"
 	"strings"
+	. "utils"
 	//"github.com/go-mix/mix" // NOTE: idk why i have this here
 )
 
 func Run(project string, singerLocations []string) error {
-	//parse the project file
+	// NOTE: singer locations should be in config file
 
+	//parse the project file
 	parsedProject, err := parser.ParseFile(project)
 	if err != nil {
-		return error
+		return err
 	}
 
-	singer := parsedProject[1][0] // TODO: wrong
+	singer := parsedProject[1][0] // TODO: wrong. basic project is more complex
 
-	loadSinger(singer, singerLocations)
+	_ := loadSinger(singer, singerLocations)
 
 	return nil
 }
 
-func loadSinger(name string, locations []string) {
-	Println("Loading " + name + " from " + strings.Join(locations, ", ") + "...")
+func loadSinger(singer Singer, locations []string) any {
+	Println("Loading " + singer.name + " from " + strings.Join(locations, ", ") + "...")
 
 	// maybe i'll change it so that main loads the singer and this decodes it
 	// like flac --> decoded flac
+
+	return nil
 }
